ddb92585d1d305c1b09359810ebe2a55a56a8c40
danielxz331 <danielxz331@gmail.com>
2023-05-21 10:33:18 -0500

    Corrección usuario postgres en docker y adicion de login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,76 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+Use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+
+    public function register(Request $request)
+    {
+        //validacion de los datos
+        $request->validate([
+            'name' => 'required',
+            'tipo_usuario' => 'required',
+            'ruta_imagen_usuario' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+        //alta del usuario
+        $user = new User();
+        $user->name = $request->name;
+        $user->tipo_usuario = $request->tipo_usuario;
+        $user->ruta_imagen_usuario = $request->ruta_imagen_usuario;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        //respuesta
+        // return response()->json([
+        //     "message" => "Metodo register ok"
+        // ]);
+        return response()->json($user);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+            $cookie = cookie('cookie_token', $token, 60*24);
+            return response(["token"=>$token], Response::HTTP_ACCEPTED)->withCookie($cookie);
+        } else {
+            return response(['message' => 'credenciales invalidas'], Response::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function userProfile(Request $request)
+    {
+        return response()->json([
+            "message" => "userProfile ok",
+            "userData" => auth()->user()
+        ], Response::HTTP_OK);
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('cookie_token');
+        return response(["message"=>"Cierre de sesion OK"], Response::HTTP_OK)->withCookie($cookie);
+    }
+
+    public function allUsers()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\UserController;
+use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +17,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+//Rutas productos
+
 Route::get('/productos', [ProductoController::class, 'index']);
 Route::post('/productos', [ProductoController::class, 'store']); 
 Route::put('/productos/{id}', [ProductoController::class, 'update']);
 Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);
 
+//Rutas usuarios
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/allusers', [UserController::class, 'allUsers']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('user-profile', [UserController::class, 'userProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
